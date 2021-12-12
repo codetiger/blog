c0f2603bba9e2ba6b6e522fe5a929f8ec6988452
codetiger <nharishankar@gmail.com>
2021-12-12 13:05:31 +0530

    Fixed the sitemap xsl link issue

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 rm -rf docs
 gssg --url https://codetiger.github.io --dest docs --subDir blog
 cp google* docs/
+find ./docs -iname "*.xml" -type f -exec sed -i '' 's/codetiger\.github\.io\/sitemap\.xsl/codetiger\.github\.io\/blog\/sitemap\.xsl/g' {} \;
