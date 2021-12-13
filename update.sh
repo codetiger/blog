36497c9761cae76fadeffd6791c1365c33d3dbef
codetiger <nharishankar@gmail.com>
2021-12-13 15:54:18 +0530

    Added minify for all files

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
-rm -rf docs
-gssg --url https://codetiger.github.io --dest docs --subDir blog
-cp google* docs/
-cp robots.txt docs/
-find ./docs -iname "*.xml" -type f -exec sed -i '' 's/codetiger\.github\.io\/sitemap\.xsl/codetiger\.github\.io\/blog\/sitemap\.xsl/g' {} \;
+rm -rf raw docs
+gssg --url https://codetiger.github.io --dest raw --subDir blog
+cp google* raw/
+cp robots.txt raw/
+find ./raw -iname "*.xml" -type f -exec sed -i '' 's/codetiger\.github\.io\/sitemap\.xsl/codetiger\.github\.io\/blog\/sitemap\.xsl/g' {} \;
+minify -r -o ./ raw
+mv raw docs
