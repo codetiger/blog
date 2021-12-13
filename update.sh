ba94c4379646c30b87f1ebffb29ec431dd3ae804
codetiger <nharishankar@gmail.com>
2021-12-13 14:19:59 +0530

    Added robots.txt and sitemap fixes

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 rm -rf docs
 gssg --url https://codetiger.github.io --dest docs --subDir blog
 cp google* docs/
+cp robots.txt docs/
 find ./docs -iname "*.xml" -type f -exec sed -i '' 's/codetiger\.github\.io\/sitemap\.xsl/codetiger\.github\.io\/blog\/sitemap\.xsl/g' {} \;
