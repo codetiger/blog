aaf346b072ce4abd7fc9138524b30cc86e92575b
codetiger <nharishankar@gmail.com>
2022-12-26 15:18:36 +0530

    New post on Bucharest

--- update.sh
+++ update.sh
@@ -3,5 +3,9 @@ gssg --url https://codetiger.github.io --dest raw --subDir blog
 cp google* raw/
 cp robots.txt raw/
 find ./raw -iname "*.xml" -type f -exec sed -i '' 's/codetiger\.github\.io\/sitemap\.xsl/codetiger\.github\.io\/blog\/sitemap\.xsl/g' {} \;
-minify -r -o ./ raw
+
+find ./raw -iname "*.css" -type f -exec cssnano {} {} \;
+find ./raw -iname "*.html" -type f -exec html-minifier --collapse-whitespace {} -o {} \;
+find ./raw -iname "*.js" -type f -exec uglifyjs --compress --mangle -o {} -- {} \;
+
 mv raw docs
