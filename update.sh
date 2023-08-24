202b8ff53d2b9d141de949f551d1f1e755a0eb01
codetiger <nharishankar@gmail.com>
2023-08-24 11:43:37 +0530

    Fixed issue in blog theme

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 rm -rf raw docs
-gssg --url https://codetiger.github.io --dest raw --subDir blog
+# gssg --url https://codetiger.github.io --dest raw --subDir blog
+gssg --domain http://localhost:2368 --url "https://codetiger.github.io/" --dest raw --subDir blog
 cp google* raw/
 cp robots.txt raw/
 find ./raw -iname "*.xml" -type f -exec sed -i '' 's/codetiger\.github\.io\/sitemap\.xsl/codetiger\.github\.io\/blog\/sitemap\.xsl/g' {} \;
